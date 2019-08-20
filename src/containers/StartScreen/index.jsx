36dea5cc5f3150fc0fa3643fcf809b9f2d71884a
coolday <cooldaynow@gmail.com>
2019-08-20 18:16:55 +0300

    change startScreen

--- src/containers/StartScreen/index.jsx
+++ src/containers/StartScreen/index.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import Ball from '../../components/Ball';
 import {start, changeColor} from '../../actions';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faPlayCircle} from '@fortawesome/free-solid-svg-icons';
 import './index.scss';
 
 const StartScreen = ({start, player, changeColor, color}) => (
   <div className="screen__wrap">
-    <h1> 4 In A Row </h1>
+    <div className="screen__header">
+      <div className="left balls" />
+      <h1> 4 In A Row </h1>
+      <div className="right balls" />
+    </div>
     <div className="screen__buttons">
-      <button onClick={start}> START </button>
+      <button className="start__button" onClick={start}>
+        <FontAwesomeIcon icon={faPlayCircle} />{' '}
+      </button>
     </div>
   </div>
 );
