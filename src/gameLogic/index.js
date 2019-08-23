c568bb98b695c2788baf1ce8553e5e4390dab2b9
coolday <cooldaynow@gmail.com>
2019-08-23 16:48:59 +0300

    change logic

--- src/gameLogic/index.js
+++ src/gameLogic/index.js
@@ -1,10 +1,13 @@
 export let winCoords = [];
+let winCoordsRight = [];
+let winCoordsLeft = [];
 const square = 4;
 const checkWin = (x, arr) => {
   for (let col = 0, len = 4; col < len; col++) {
     for (let row = 0, len = 4; row < len; row++) {
       if (checkDiagonal(x, col, row, arr) || checkLanes(x, col, row, arr)) {
         //   console.log('checkDiagonal work');
+        console.log(winCoords);
         return true;
       }
     }
@@ -13,17 +16,33 @@ const checkWin = (x, arr) => {
 const checkDiagonal = (x, offsetX, offsetY, arr) => {
   let toRight = true;
   let toLeft = true;
+  winCoordsLeft = [];
+  winCoordsRight = [];
   winCoords = [];
   for (let i = offsetX, len = square + offsetX; i < len; i++) {
     toRight = toRight && arr[i][i - offsetX + offsetY] === x;
     toLeft = toLeft && arr[arr.length - i - 1][i - offsetX + offsetY] === x;
     if (toRight) {
-      winCoords.push([i, i - offsetX + offsetY]);
-    } else if (toLeft) {
-      winCoords.push([arr.length - i - 1, i - offsetX + offsetY]);
+      //console.log('to right');
+      winCoordsRight.push([i, i - offsetX + offsetY]);
+      //console.log('toRIght', winCoords);
+    }
+    if (toLeft) {
+      // winCoords.push([arr.length - i - 1, i - offsetX + offsetY]);
+      //console.log('toLeft', winCoords);
+      winCoordsLeft.push([arr.length - i - 1, i - offsetX]);
     }
   }
   if (toRight || toLeft) {
+    if (toRight) {
+      //console.log('toRight', winCoords);
+      winCoords = winCoordsRight;
+    }
+    if (toLeft) {
+      // console.log('toLeft', winCoords);
+      winCoords = winCoordsLeft;
+    }
+
     return true;
   }
   return false;
@@ -32,7 +51,6 @@ const checkLanes = (x, offsetX, offsetY, arr) => {
   for (let i = offsetX, len = square + offsetX; i < len; i++) {
     let rows = true;
     let cols = true;
-    winCoords = [];
     for (let j = offsetY, len = square + offsetY; j < len; j++) {
       cols = cols && arr[i][j] === x;
       rows = rows && arr[j][i] === x;
@@ -40,7 +58,9 @@ const checkLanes = (x, offsetX, offsetY, arr) => {
     }
     if (cols || rows) {
       if (cols) {
+        console.log('cols');
       } else if (rows) {
+        console.log('rows');
         winCoords.map(el => el.reverse());
       }
       //console.log('vertical || horizontal');
